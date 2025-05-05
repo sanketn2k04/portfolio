532b5eb6fa4863da1109108d3b600c4f53fc8999
Sanket Nabade <sanketnabade2004@gmail.com>
2025-05-05 19:13:44 +0530

    feat: update layout metadata and add favicon icons for improved SEO and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,44 @@ const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const calistoga = Calistoga({ subsets: ["latin"], variable: "--font-serif", weight: "400" });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://sanketnabade.tech/'),
   title: "Sanket Nabade",
   description: "Sanket Nabade Portfolio",
+  icons: {
+    icon: [
+      {
+        url: '/assets/memoji-smile.png',
+        sizes: '32x32',
+        type: 'image/png'
+      },
+      {
+        url: '/assets/memoji-smile.png',
+        sizes: '16x16',
+        type: 'image/png'
+      },
+      {
+        url: '/assets/memoji-smile.png',
+        sizes: '180x180',
+        type: 'image/png'
+      }
+    ],
+    apple: [
+      {
+        url: '/assets/memoji-smile.png',
+        sizes: '180x180',
+        type: 'image/x-icon'
+      }
+    ]
+  },
   openGraph: {
-
-    images: '/public/assets/memoji-smile.png',
-
+    title: 'Sanket Nabade',
+    description: 'Sanket Nabade Portfolio',
+    images: [{
+      url: '/assets/memoji-smile.png',
+      width: 180,
+      height: 180,
+      alt: 'Sanket Nabade'
+    }],
   },
 };
 
@@ -25,6 +57,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/assets/memoji-smile.png" />
+      </head>
       <body
         className={twMerge(
           inter.variable,
